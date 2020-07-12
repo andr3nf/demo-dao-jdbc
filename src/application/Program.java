04508faab34c77746be26651f154962c0703f977
andr3nf <nunesfigueiredo@gmail.com>
2020-07-12 20:59:36 -0300

    Implementação do método findByDepartment da classe SellerSeller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,6 +18,13 @@ public class Program {
 		//Department obj = new Department(1, "Books");
 		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
+		
+		System.out.println("\n=== TESTE 02 - seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj: list) {
+			System.out.println(obj);
+		}
 
 	}
 
