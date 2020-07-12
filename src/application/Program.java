5e3da35ea0cb65af66f3b9d8088aee89e1e411f4
andr3nf <nunesfigueiredo@gmail.com>
2020-07-12 19:44:48 -0300

    Refatorando a intanciação no findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,10 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TESTE 01 - seller findById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		//Department obj = new Department(1, "Books");
 		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
 		System.out.println(seller);
 
 	}
