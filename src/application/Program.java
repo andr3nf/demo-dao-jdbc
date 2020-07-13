b7c543fdf1263e4e5ca7641f24b41c0f07741470
andr3nf <nunesfigueiredo@gmail.com>
2020-07-12 22:07:42 -0300

    Implementação do método delete da classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -43,6 +46,15 @@ public class Program {
 		seller.setName("André Nunes");
 		sellerDao.update(seller);
 		System.out.println("Atualizado!");
+		
+		System.out.println("\n=== TESTE 06 - seller delete ===");
+		System.out.println("Entre com um id para ser deletado:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deletado!");
+		
+		sc.close();
+		
 	}
 
 }
